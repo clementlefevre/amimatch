a06db8895748c3960d2e10af91f509f6379eb31b
clementlefevre <clement.san@gmai.com>
2016-06-03 11:18:32 +0200

    first cococomiiittt

---
+++ app/main/views.py
@@ -0,0 +1,32 @@
+from flask import render_template, abort, request, \
+    current_app
+from flask.ext.sqlalchemy import get_debug_queries
+
+from . import main
+
+
+@main.after_app_request
+def after_request(response):
+    for query in get_debug_queries():
+        if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+            current_app.logger.warning(
+                'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
+                % (query.statement, query.parameters, query.duration,
+                   query.context))
+    return response
+
+
+@main.route('/shutdown')
+def server_shutdown():
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
