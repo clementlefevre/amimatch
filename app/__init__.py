978fa266f89cd0e572726913a74724747601909f
clementlefevre <clement.san@gmai.com>
2016-06-09 23:06:26 +0200

    Implement Angular JS.

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,8 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
-api = Api()
+
 pagedown = PageDown()
-
-
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -43,7 +41,5 @@ def create_app(config_name):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
-
 
     return app
